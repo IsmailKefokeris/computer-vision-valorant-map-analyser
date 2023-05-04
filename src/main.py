04ad764afe1b9e25d988db36689ecc84f02f609b
ismailkefokeris <ismailkefokeris@gmail.com>
2023-05-04 04:22:18 +0100

    Changed Taskbar icon to application ICon (WIndows Only)

--- src/main.py
+++ src/main.py
@@ -2,12 +2,20 @@ from PySide6.QtWidgets import QApplication
 
 from ui.controllers.gui_interface import MainWindow
 import sys
+import os
 
 # UI Functions
 from ui.controllers.ui_functions import *
 
 
 if __name__ == "__main__":
+    try:
+        from ctypes import windll  # Only exists on Windows.
+        myappid = 'IsmailKefokeris.CVVMA.030'
+        windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+    except ImportError:
+        pass
+
     app = QApplication(sys.argv)
 
     window = MainWindow()
